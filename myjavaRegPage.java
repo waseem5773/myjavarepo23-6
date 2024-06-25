2ca0b6e663bb8db38d88d72de80be652edd6c544
waseem5773 <mohammedwaseem432000@gmail.com>
2024-06-25 19:29:16 +0530

    my manager work

--- myjavaRegPage.java
+++ myjavaRegPage.java
@@ -2,4 +2,11 @@
 2nd code line of java by dev B
 3rd code line of java by dev A
 
+<<<<<<< Updated upstream
 completed manager work
+=======
+1st day code of line completed
+2nd day code of line completed
+3rd day code of line completed
+4th day code of line completed
+5th day code of line completed
